97281397b6ca5fbe3521c5636743ba5276f7634e
Axel Kohlmeyer <akohlmey@gmail.com>
2023-03-04 15:57:14 -0500

    use docs folder

--- utils/publish.sh
+++ utils/publish.sh
@@ -3,7 +3,8 @@
 test -x html || exit 1
 git branch --show-current > .current-branch
 git checkout github-docs
-git add html
+cp -ar html/* html/.* docs
+git add -A docs
 git commit -m 'update website'
 git push origin github-docs
 git checkout `cat .current-branch`
